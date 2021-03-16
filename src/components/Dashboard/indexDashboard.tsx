96614322144f2f5f9a4c3b27184efd27145e6193
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-03-16 11:59:29 -0300

    Create TransactionsTable component

--- src/components/Dashboard/indexDashboard.tsx
+++ src/components/Dashboard/indexDashboard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Summary } from '../Summary/indexSummary';
+import { TransactionsTable } from '../TransactionsTable/indexTransactionsTable';
 import { Container } from './style';
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
