5903adf9e1cb26a7dd2f38d7ecd5cd46b988148b
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-03-16 10:28:06 -0300

    Add Poppins font to the body

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -36,12 +36,22 @@ html{
     -webkit-font-smoothing:antialiased;
   }
 
+  body,button,input,textarea{
+    font-family: 'Poppins', sans-serif;
+    font-weight:400;
+  }
+
+  h1,h2,h3,h4,h5,h6, strong{
+    font-weight:600;
+  }
+
   button{
     cursor: pointer;
   }
 
   [disabled]{
     opacity:0.6;
+    cursor:not-allowed;
   }
 
 
